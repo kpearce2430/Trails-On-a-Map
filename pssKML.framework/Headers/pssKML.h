59c529011928339742d47da1af1526b0d077177c
KEITH E PEARCE <pearcek@keith-pearces-imac.local>
2014-03-29 17:05:27 -0400

    committed

---
+++ pssKML.framework/Headers/pssKML.h
@@ -0,0 +1,22 @@
+//
+//  pssKML.h
+//  pssKML
+//
+//  Created by KEITH E PEARCE  on 3/26/14.
+//  Copyright (c) 2014 Pearce Software Solutions. All rights reserved.
+//
+//
+//  Framework based on:  https://github.com/jverkoey/iOS-Framework
+//  Origianl KML Code:  https://github.com/FLCLjp/iOS-KML-Framework.git
+//  Original TBXML Code:  https://github.com/71squared/TBXML
+//
+//
+#import <Foundation/Foundation.h>
+
+#import "pssKMl/KML.h"
+
+@interface pssKML : NSObject
+
+- (void) pssKMLMessage ;
+
+@end
