c84b65efc0a113b82ad7ba977cf790c8db7c5bd1
KEITH E PEARCE <pearcek@keith-pearces-imac.local>
2014-05-19 10:25:05 -0400

    Added test for computing the distance in 3D (lat/long/alt).

--- Trails On a MapTests/Trails_On_a_MapTests.m
+++ Trails On a MapTests/Trails_On_a_MapTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "TOMDistance.h"
 
 @interface Trails_On_a_MapTests : XCTestCase
 
@@ -31,6 +32,33 @@
     NSString *p = nil;
     XCTAssertNil(p, @"p is nil" );
     // XCTPass(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    CLLocationCoordinate2D p1Coord = CLLocationCoordinate2DMake(34.06937608, -84.25335864);
+    CLLocationCoordinate2D p2Coord = CLLocationCoordinate2DMake(34.06969865, -84.25232029);
+    
+    // CLLocation *p1 = [[CLLocation alloc] initWithLatitude:34.06937608 longitude:-84.25335864];
+    CLLocation *p1 = [[CLLocation alloc] initWithCoordinate:p1Coord altitude:300.0f horizontalAccuracy:0.0f verticalAccuracy:0.0f timestamp:Nil];
+    
+    CLLocation *p2 = [[CLLocation alloc] initWithCoordinate:p2Coord altitude:300.0f horizontalAccuracy:0.0f verticalAccuracy:0.0f timestamp:Nil];
+    
+    CLLocation *p3 = [[CLLocation alloc] initWithCoordinate:p2Coord altitude:1300.0f horizontalAccuracy:0.0f verticalAccuracy:0.0f timestamp:Nil];
+    
+    CLLocationDistance dist = [TOMDistance distanceFrom:p1 To:p2];
+    NSLog(@"Distance:%.3f",dist);
+    XCTAssertNotEqual(dist, 0.00F, @"dist p1->p2 = Zero");
+    
+    dist = [TOMDistance distanceFrom:p2 To:p3];
+    NSLog(@"Distance:%.3f",dist);
+    XCTAssertNotEqual(dist, 0.00F, @"dist p1->p2+1000m = Zero");
+    
+    CLLocationCoordinate2D p3Coord = CLLocationCoordinate2DMake(41.756192 , -87.967360);
+    CLLocationCoordinate2D p4Coord = CLLocationCoordinate2DMake(41.758701 , -87.973307);
+    
+    p3 = [[CLLocation alloc] initWithCoordinate:p3Coord altitude:192.0f horizontalAccuracy:0.0f verticalAccuracy:0.0f timestamp:Nil];
+    CLLocation *p4 = [[CLLocation alloc] initWithCoordinate:p4Coord altitude:198.0f horizontalAccuracy:0.0f verticalAccuracy:0.0f timestamp:Nil];
+    
+    dist = [TOMDistance distanceFrom:p3 To:p4];
+    NSLog(@"P3->P4 DISTANCE: %.2F",dist);
+    XCTAssertNotEqual(dist, 0.0f, @"dist p3->p4  Zero");
 }
 
 @end
