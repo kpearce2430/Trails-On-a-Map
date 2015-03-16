a5857fbd8199d98777e520478febc9dd1bc43bf2
KEITH E PEARCE <pearcek@keith-pearces-imac.local>
2015-03-16 19:58:04 -0400

    Release 1.2

--- Trails On a Map/TOMDistance.h
+++ Trails On a Map/TOMDistance.h
@@ -7,10 +7,10 @@
 //
 
 #import <Foundation/Foundation.h>
-
-@interface TOMDistance : NSObject
 #import <MapKit/MapKit.h>
 #import "TOM.h"
+
+@interface TOMDistance : NSObject
 
 // CLLocationDistance
 + (CLLocationDistance) displayDistance: (CLLocationDistance) d;
